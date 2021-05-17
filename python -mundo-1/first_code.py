e41298dca14e8ca53ee5bdaf11c0bc3ea7546440
Gabriel Sousa Carvalhaes <82975630+gabcarvalhaes@users.noreply.github.com>
2021-05-17 09:56:59 -0300

    Add files via upload

---
+++ python -mundo-1/first_code.py
@@ -0,0 +1,7 @@
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade?')
+altura = input('Qual é a sua altura? ')
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('Olá, mundo!')
