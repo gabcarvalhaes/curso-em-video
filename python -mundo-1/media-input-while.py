4bef77b5ca6a1126dbeb6d06661657ba3dda68fd
Gabriel Sousa Carvalhaes <82975630+gabcarvalhaes@users.noreply.github.com>
2021-05-17 15:35:04 -0300

    Add files via upload

---
+++ python -mundo-1/media-input-while.py
@@ -0,0 +1,17 @@
+contador = int
+nome = str(input('Qual o nome do aluno? '))
+controle = int(input('Quantas notas deseja registrar para {}?'.format(nome)))
+contador = int
+soma = float
+media = float
+
+contador = 1
+soma = 0
+
+while contador <= controle:
+ nota = float(input('Digite a nota {}'.format(contador)))
+ soma = soma + nota
+ contador = contador + 1
+
+media = soma / controle
+print('A média de {} é igual a {}'.format(nome, media))
